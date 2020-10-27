a3506f92f9ab358ecbd52fa132b3f529b0d9f780
adkevin3307 <adkevin3307@gmail.com>
2020-10-27 19:46:24 +0800

    clean strdup memory

--- src/Process.cpp
+++ src/Process.cpp
@@ -17,6 +17,10 @@ Process::Process()
 Process::~Process()
 {
     if (this->args != NULL) {
+        for (size_t i = 0; i < this->command.size() + 1; i++) {
+            free(this->args[i]);
+        }
+
         delete[] this->args;
     }
 }
